aa89267182dec8f1c67a94aa27a5884a3cb4d80d
Christophe Gosiau <christophe.gosiau@tigron.be>
2016-03-14 20:38:15 +0100

    fix table name in migration

--- migrations/20160204_182713_customer_contact.php
+++ migrations/20160204_182713_customer_contact.php
@@ -18,7 +18,7 @@ class Migration_20160204_182713_Customer_contact extends \Skeleton\Database\Migr
 	 */
 	public function up() {
 		$db = Database::get();
-		$db->query('ALTER TABLE `invoice_ contact` RENAME TO `customer_contact`;');
+		$db->query('ALTER TABLE `invoice_contact` RENAME TO `customer_contact`;');
 		$db->query('ALTER TABLE `invoice` CHANGE `invoice_contact_id` `customer_contact_id` int(11) NOT NULL AFTER `customer_id`;');
 		$db->query('ALTER TABLE `invoice_queue` CHANGE `invoice_contact_id` `customer_contact_id` int(11) NOT NULL AFTER `customer_id`;');
 	}
