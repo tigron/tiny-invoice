a821ddd853931bd1b65adcb4a96de6266f5a74d0
David Vandemaele <david@tigron.be>
2016-04-22 15:09:49 +0200

    Various fixes to hadling incoming documents

---
+++ migrations/20160422_142133_purchase_paid.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Database migration class
+ *
+ * @author Gerry Demaret <gerry@tigron.be>
+ * @author Christophe Gosiau <christophe@tigron.be>
+ * @author David Vandemaele <david@tigron.be>
+ */
+
+use \Skeleton\Database\Database;
+
+class Migration_20160422_142133_Purchase_paid extends \Skeleton\Database\Migration {
+
+	/**
+	 * Migrate up
+	 *
+	 * @access public
+	 */
+	public function up() {
+		$db = Database::get();
+		$db->query("
+			ALTER TABLE `purchase`
+			CHANGE `paid` `paid` tinyint(1) NOT NULL AFTER `price_incl`;
+		");
+		$db->query("
+			UPDATE purchase SET paid = 1 WHERE paid IS NOT NULL
+		")
+	}
+
+	/**
+	 * Migrate down
+	 *
+	 * @access public
+	 */
+	public function down() {
+
+	}
+}
