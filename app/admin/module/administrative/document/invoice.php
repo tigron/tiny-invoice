3724840a14e1ba08a6e5803e32957290325576e0
Christophe Gosiau <christophe.gosiau@tigron.be>
2016-05-04 18:05:10 +0200

    search is not working for documents

--- app/admin/module/administrative/document/invoice.php
+++ app/admin/module/administrative/document/invoice.php
@@ -43,6 +43,11 @@ class Web_Module_Administrative_Document_Invoice extends Web_Module_Administrati
 				}
 			}
 		}
+
+		if (isset($_POST['search'])) {
+			$pager->set_search($_POST['search']);
+		}
+
 		if (count($tag_ids) > 0) {
 			$pager->add_join('document_tag', 'document_id', 'document.id');
 			$pager->add_condition('document_tag.tag_id', 'IN', $tag_ids);
