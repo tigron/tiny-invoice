a610fe4955a75f2a7291eee88b2ed4ca8ea323cf
Christophe Gosiau <christophe@tigron.be>
2016-05-12 22:07:11 +0200

    fix migrations

--- migrations/20160425_114833_document_migrate.php
+++ migrations/20160425_114833_document_migrate.php
@@ -18,29 +18,29 @@ class Migration_20160425_114833_Document_migrate extends \Skeleton\Database\Migr
 	 */
 	public function up() {
 		$db = Database::get();
-		
-		$db->query("ALTER TABLE `document` ADD `updated` datetime NULL;");
+
+//		$db->query("ALTER TABLE `document` ADD `updated` datetime NULL;");
 		$db->query('UPDATE document SET classname = ?', [ 'Document' ]);
 
 		$ids = $db->get_column('SELECT id FROM document');
+
 		foreach ($ids as $id) {
 			$updated = null;
-			$document = Document::get_by_id($id);
-			if ($document->is_purchase() === true) {
-				$document = $document->change_classname('Document_Incoming_Invoice');
-
-				$purchase = Purchase::get_by_document($document);
-				$document->supplier_id = $purchase->supplier_id;
-				$document->price_incl = $purchase->price_incl;
-				$document->price_excl = $purchase->price_excl;
-				$document->paid = $purchase->paid;
-				$document->date = $purchase->date;
-				$document->expiration_date = $purchase->expiration_date;
-				$updated = $purchase->updated;
-
-				$document->save();
+			$row = $db->get_row('SELECT * FROM purchase WHERE document_id=?', [ $id ]);
+			if ($row === null) {
+				continue;
 			}
+			$document = Document::get_by_id($id);
+			$document = $document->change_classname('Document_Incoming_Invoice');
 
+			$document->supplier_id = $row['supplier_id'];
+			$document->price_incl = $row['price_incl'];
+			$document->price_excl = $row['price_excl'];
+			$document->paid = $row['paid'];
+			$document->date = $row['date'];
+			$document->expiration_date = $row['expiration_date'];
+			$updated = $row['updated'];
+			$document->save();
 			$db->query('UPDATE document SET updated = ? WHERE id = ?', [ $updated, $document->id ]);
 		}
 
