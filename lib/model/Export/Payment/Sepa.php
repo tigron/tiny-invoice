68231b857b2a25deceae5e5c54447fbd55c577fb
Christophe Gosiau <christophe.gosiau@tigron.be>
2016-09-16 14:34:47 +0200

    New version of SEPA payment export

--- lib/model/Export/Payment/Sepa.php
+++ lib/model/Export/Payment/Sepa.php
@@ -7,8 +7,6 @@
  * @author David Vandemaele <david@tigron.be>
  */
 
-use Sepa\TransferFile;
-
 class Export_Payment_Sepa extends Export {
 
 	/**
@@ -31,58 +29,73 @@ class Export_Payment_Sepa extends Export {
 			$total_price += $document->price_incl;
 		}
 
-		$sepaFile = new TransferFile();
-		$sepaFile->messageIdentification = $this->id;
-		$sepaFile->initiatingPartyName = Setting::get_by_name('company')->value;
 
+		$organization = new \Tigron\Sepa\Organization();
+		$organization->name = Setting::get_by_name('company')->value;
+		/**
+		 * Set the identification
+		 *
+		 * Possible types: bic, bei or kbo-bce
+		 */
+		$organization->set_identification('kbo-bce', Setting::get_by_name('vat')->value);
+
+		$credit = new \Tigron\Sepa\File\Credit();
+		$credit->messageIdentification = $this->id;
+		$credit->initiatingParty = $organization;
+
+		$debtor = new \Tigron\Sepa\Debtor();
+		$debtor->name = Setting::get_by_name('company')->value;
+		$debtor->country = Country::get_by_id(Setting::get_by_name('country_id')->value)->iso2;
+		$debtor->zipcode = Setting::get_by_name('zipcode')->value;
+		$debtor->city = Setting::get_by_name('city')->value;
+		$debtor->street = Setting::get_by_name('street')->value;
+		$debtor->housenumber = Setting::get_by_name('housenumber')->value;
 
 		foreach ($documents as $document) {
-			/*
-			* Add a payment to the SEPA file. This method
-			* may be called more than once to add multiple
-			* payments to the same file.
-			*/
-			$payment1 = $sepaFile->addPaymentInfo(array(
-				'id'                    => $document->id,
-				'debtorName'            => Setting::get_by_name('company')->value,
-				'debtorAccountIBAN'     => Setting::get_by_name('iban')->value,
-				'debtorAgentBIC'        => strtoupper(Setting::get_by_name('bic')->value)
-			//  'debtorAccountCurrency' => 'GPB', // optional, defaults to 'EUR'
-			//  'categoryPurposeCode'   => 'SUPP', // optional, defaults to NULL
-			));
+			// Create a payment
+			$payment = new \Tigron\Sepa\Payment();
+			$payment->paymentInformationIdentification = 'document_' . $document->id;
 
-			/*
-			* Add a credit transfer to the payment. This method
-			* may be called more than once to add multiple
-			* transfers for the same payment.
-			*/
-			if ($document->payment_structured_message != '') {
-				$message = str_replace('/', '', $document->payment_structured_message);
+			$document_date = new DateTime($document->date);
+			$now = new DateTime();
+
+			if ($document_date < $now) {
+				$document_date = $now;
+			}
+
+			$payment->requestedExecutionDate = $document_date;
+			$payment->debtorAccount = Setting::get_by_name('iban')->value;
+			$payment->debtorAgent = Setting::get_by_name('bic')->value;
+			$payment->debtor = $debtor;
+
+			// Create a transaction
+			$supplier = $document->supplier;
+			$creditor = new \Tigron\Sepa\Creditor();
+			$creditor->name = $supplier->company;
+			$creditor->country = $supplier->country->iso2;
+			$creditor->zipcode = $supplier->zipcode;
+			$creditor->city = $supplier->city;
+			$creditor->street = $supplier->street;
+			$creditor->housenumber = $supplier->housenumber;
+
+			$transaction = new \Tigron\Sepa\Transaction();
+			$transaction->paymentIdentification = 'document_' . $document->id;
+			$transaction->amount = $document->price_incl;
+			$transaction->creditorAgent = $supplier->bic;
+			$transaction->creditorAccount = $supplier->iban;
+			$transaction->creditor = $creditor;
+			if ($document->payment_message != '') {
+				$transaction->unstructured_message = $document->payment_message;
 			} else {
-				$message = $document->payment_message;
+				$transaction->structured_message = $document->payment_structured_message;
 			}
-			$payment1->addCreditTransfer(array(
-				'id'                    => $document->id,
-				'currency'              => 'EUR',
-				'amount'                => $document->price_incl, // or as float: 0.02 or as integer: 2
-				'creditorBIC'           => strtoupper($document->supplier->bic),
-				'creditorName'          => $document->supplier->company,
-				'creditorAccountIBAN'   => $document->supplier->iban,
-				'remittanceInformation' => $message,
-			));
+
+			$payment->transactions[] = $transaction;
+			$credit->payments[] = $payment;
 		}
 
-		/* Generate the file and return the XML string. */
-		$content = $sepaFile->asXML();
-
-		if ($data['mark_paid']) {
-			foreach ($documents as $document) {
-				$document->paid = true;
-				$document->save();
-			}
-		}
-
-		$file = \Skeleton\File\File::store('payment_sepa' . date('Ymd') . '.xml', $content);
+		$xml = $credit->render();
+		$file = \Skeleton\File\File::store('payment_sepa' . date('Ymd') . '.xml', $xml);
  		$this->file_id = $file->id;
 		$this->save();
 	}
