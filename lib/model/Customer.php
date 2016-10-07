36442d377ac7431071ef874645a698c9f71a4ebe
Christophe Gosiau <christophe.gosiau@tigron.be>
2016-10-07 22:26:20 +0200

    improvement: create 'get_display_name()' this should prevent to duplicate logic for showing the name in each template

--- lib/model/Customer.php
+++ lib/model/Customer.php
@@ -47,6 +47,27 @@ class Customer {
 	}
 
 	/**
+	 * Get display_name
+	 *
+	 * @access public
+	 * @return string $display_name
+	 */
+	public function get_display_name() {
+		$display_name = '';
+		if (!empty($this->details['company'])) {
+			$display_name .= $this->details['company'];
+
+			if (!empty($this->details['firstname']) or !empty($this->details['lastname'])) {
+				$display_name .= ' (' . $this->details['firstname'] . ' ' . $this->details['lastname'] . ')';
+			}
+
+			return $display_name;
+		}
+
+		return $this->details['firstname'] . ' ' . $this->details['lastname'];
+	}
+
+	/**
 	 * Get active invoice contacts
 	 *
 	 * @access public
