6e5e708c4825ea19c2b6edbc7b5b5c8d18b8a730
Christophe Gosiau <christophe@tigron.be>
2016-10-10 15:15:18 +0200

    set all tables in innodb/utf8

---
+++ migrations/20161010_151206_utf8_innodb_baby.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Database migration class
+ *
+ * @author Gerry Demaret <gerry@tigron.be>
+ * @author Christophe Gosiau <christophe@tigron.be>
+ * @author David Vandemaele <david@tigron.be>
+ */
+
+use \Skeleton\Database\Database;
+
+class Migration_20161010_151206_Utf8_innodb_baby extends \Skeleton\Database\Migration {
+
+	/**
+	 * Migrate up
+	 *
+	 * @access public
+	 */
+	public function up() {
+		$db = Database::get();
+		$tables = $db->get_column('SHOW tables;', []);
+		foreach ($tables as $table) {
+			$db->query("
+				ALTER TABLE `" . $table . "`
+				ENGINE='InnoDB' COLLATE 'utf8_unicode_ci';
+			", []);
+		}
+	}
+
+	/**
+	 * Migrate down
+	 *
+	 * @access public
+	 */
+	public function down() {
+
+	}
+}
