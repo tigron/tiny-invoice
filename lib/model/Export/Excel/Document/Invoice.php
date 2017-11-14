ccc28e2112baa9447f2962fdf4d2be71b2b7b6ae
tigron <tigron@test02.int.tigron.net>
2017-11-14 16:35:05 +0100

    bugfix: when exporting to excel, make sure the same sorting permissions are applied as for the pager in the module

--- lib/model/Export/Excel/Document/Invoice.php
+++ lib/model/Export/Excel/Document/Invoice.php
@@ -19,19 +19,25 @@ class Export_Excel_Document_Invoice extends Export_Expertm {
 	public function run() {
 		\Skeleton\Pager\Config::$items_per_page = 1000;
 		$pager = Pager::get_by_options_hash($this->get_data());
+		$pager->add_sort_permission('id');
+		$pager->add_sort_permission('document.date');
+		$pager->add_sort_permission('title');
+		$pager->add_sort_permission('document_incoming_invoice.paid');
+		$pager->add_sort_permission('document_incoming_invoice.accounting_identifier');
+		$pager->add_sort_permission('document_incoming_invoice.expiration_date');
+		$pager->add_sort_permission('supplier.company');
+		$pager->add_sort_permission('document_incoming_invoice.price_incl');
 		$pager->page();
 		$invoices = $pager->items;
 
 		$excel = new PHPExcel();
-		$headers = ['Document number', 'Created', 'Supplier', 'Price excl', 'Price incl', 'Paid'];
+		$headers = ['Document number', 'Created', 'Supplier', 'Title', 'Price excl', 'Price incl', 'Paid'];
 
 		$worksheet = $excel->getActiveSheet();
 
 		foreach($headers as $key => $header) {
 			$worksheet->setCellValueByColumnAndRow($key, 1, $header);
 		}
-
-
 
 		$row = 1;
 		foreach ($invoices as $invoice) {
@@ -40,15 +46,15 @@ class Export_Excel_Document_Invoice extends Export_Expertm {
 			$worksheet->setCellValueByColumnAndRow(0, $row, $invoice->id);
 			$worksheet->setCellValueByColumnAndRow(1, $row, $invoice->created);
 			$worksheet->setCellValueByColumnAndRow(2, $row, $invoice->supplier->company);
-			$worksheet->setCellValueByColumnAndRow(3, $row, $invoice->price_excl);
-			$worksheet->setCellValueByColumnAndRow(4, $row, $invoice->price_incl);
+			$worksheet->setCellValueByColumnAndRow(3, $row, $invoice->title);
+			$worksheet->setCellValueByColumnAndRow(4, $row, $invoice->price_excl);
+			$worksheet->setCellValueByColumnAndRow(5, $row, $invoice->price_incl);
 			if ($invoice->paid) {
-				$worksheet->setCellValueByColumnAndRow(5, $row, 'Yes');
+				$worksheet->setCellValueByColumnAndRow(6, $row, 'Yes');
 			} else {
-				$worksheet->setCellValueByColumnAndRow(5, $row, 'No');
+				$worksheet->setCellValueByColumnAndRow(6, $row, 'No');
 			}
 		}
-
 
 		$writer = new PHPExcel_Writer_Excel2007($excel);
 		ob_start();
