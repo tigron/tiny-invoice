f43d45f1618ba086e039887b71765418d7764ee3
tigron <tigron@test02.int.tigron.net>
2017-11-14 16:29:10 +0100

    bugfix: convert incoming without subject to document gives a validation exception

--- app/admin/module/administrative/incoming.php
+++ app/admin/module/administrative/incoming.php
@@ -65,6 +65,11 @@ class Web_Module_Administrative_Incoming extends Module {
 		$template->assign('incoming', $incoming);
 	}
 
+	/**
+	 * Rotate a page
+	 *
+	 * @access public
+	 */
 	public function display_rotate_page() {
 		$page = Incoming_Page::get_by_id($_GET['id']);
 		$page->rotate();
@@ -101,7 +106,11 @@ class Web_Module_Administrative_Incoming extends Module {
 		}
 
 		$document = new Document();
-		$document->title = $incoming->subject;
+		if ($incoming->subject == '') {
+			$document->title = 'Untitled document';
+		} else {
+			$document->title = $incoming->subject;
+		}
 		$document->file_id = $pdf->id;
 		$document->classname = 'Document';
 		$document->save();
