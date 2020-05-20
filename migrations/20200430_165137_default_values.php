9b37a67715c85b418afafbf6bac5bfca3e501ef5
Jochen Timmermans <jochen.timmermans@tigron.be>
2020-05-20 17:07:36 +0200

    F#20304 Move migration queries for `file` table changes to `skeleton-file` package

--- migrations/20200430_165137_default_values.php
+++ migrations/20200430_165137_default_values.php
@@ -87,20 +87,6 @@ class Migration_20200430_165137_Default_values extends \Skeleton\Database\Migrat
 		", []);
 
 		$db->query("
-			ALTER TABLE `file`
-			CHANGE `path` `path` varchar(255) COLLATE 'utf8_unicode_ci' NULL AFTER `name`,
-			CHANGE `mime_type` `mime_type` varchar(255) COLLATE 'utf8_unicode_ci' NULL AFTER `md5sum`,
-			CHANGE `size` `size` bigint(20) NULL AFTER `mime_type`,
-			CHANGE `deleted` `deleted` DATETIME NULL AFTER `created`;
-		", []);
-
-		$db->query("
-			UPDATE `file`
-			SET `deleted` = NULL
-			WHERE `deleted` = '0000-00-00 00:00:00'
-		", []);
-
-		$db->query("
 			ALTER TABLE `invoice`
 			CHANGE `file_id` `file_id` int(11) NULL AFTER `customer_contact_id`,
 			CHANGE `paid` `paid` tinyint(4) DEFAULT 0 AFTER `customer_contact_id`,
