5682ccfd1d29d3436743ed4af2e03453a5986bc1
Christophe Gosiau <christophe@tigron.be>
2021-02-03 11:37:15 +0100

    upgrade coda parser

--- lib/model/Bank/Account/Statement/Parser/Coda.php
+++ lib/model/Bank/Account/Statement/Parser/Coda.php
@@ -33,25 +33,28 @@ class Bank_Account_Statement_Parser_Coda extends Bank_Account_Statement_Parser {
 	 * @param \Skeleton\File\File $file
 	 */
 	public function parse(\Skeleton\File\File $file) {
-		$parser = new Parser();
+		$parser = new Codelicious\Coda\Parser();
 		$statements = $parser->parseFile($file->get_path(), 'raw');
+print_r($statements);
 
 		foreach ($statements as $statement) {
+			$statement_account = $statement->getAccount();
 			try {
-				$bank_account = Bank_Account::get_by_number($statement->original_situation->account_number);
+				$bank_account = Bank_Account::get_by_number($statement_account->getNumber());
 			} catch (Exception $e) {
 				$bank_account = new Bank_Account();
 			}
-			$bank_account->number = trim($statement->original_situation->account_number);
-			$bank_account->description = $statement->original_situation->account_description;
-			$bank_account->name = $statement->original_situation->account_name;
-			$bank_account->bic = $statement->identification->account_bic;
+			$bank_account->number = trim($statement_account->getNumber());
+			$bank_account->description = $statement_account->getDescription();
+			$bank_account->name = $statement_account->getName();
+			$bank_account->bic = $statement_account->getBic();
 			$bank_account->from_coda = true;
 			$bank_account->save();
 
-			$statement_sequence_number = $statement->original_situation->statement_sequence_number;
-			$year = date('Y', strtotime($statement->identification->creation_date));
-			$month = date('m', strtotime($statement->identification->creation_date));
+			$statement_sequence_number = $statement->getSequenceNumber();
+			$year = $statement->getDate()->format('Y');
+			$month = $statement->getDate()->format('m');
+
 			if ($month == 12 AND $statement_sequence_number < 20) {
 				$year++;
 			}
@@ -67,54 +70,36 @@ class Bank_Account_Statement_Parser_Coda extends Bank_Account_Statement_Parser {
 				$bank_account_statement->bank_account_id = $bank_account->id;
 				$bank_account_statement->sequence = $statement_sequence_number;
 			}
-			$bank_account_statement->original_situation_date = $statement->original_situation->date;
-			$bank_account_statement->original_situation_balance = $statement->original_situation->balance;
-			$bank_account_statement->new_situation_date = $statement->new_situation->date;
-			$bank_account_statement->new_situation_balance = $statement->new_situation->balance;
+			$bank_account_statement->original_situation_date = $statement->getDate()->format('Y-m-d');
+			$bank_account_statement->original_situation_balance = $statement->getInitialBalance();
+			$bank_account_statement->new_situation_date = $statement->getNewBalance();
+			$bank_account_statement->new_situation_balance = $statement->getNewDate()->format('Y-m-d');
 			$bank_account_statement->save();
 
 
-			foreach ($statement->transactions as $transaction) {
+			foreach ($statement->getTransactions() as $transaction) {
 				try {
-					$bank_account_statement_transaction = Bank_Account_Statement_Transaction::get_by_bank_account_statement_sequence($bank_account_statement, $transaction->line21->sequence_number);
+					$bank_account_statement_transaction = Bank_Account_Statement_Transaction::get_by_bank_account_statement_sequence($bank_account_statement, $transaction->getTransactionSequence());
 				} catch (Exception $e) {
 					$bank_account_statement_transaction = new Bank_Account_Statement_Transaction();
 					$bank_account_statement_transaction->bank_account_statement_id = $bank_account_statement->id;
-					$bank_account_statement_transaction->sequence = $transaction->line21->sequence_number;
+					$bank_account_statement_transaction->sequence = $transaction->getTransactionSequence();
 				}
-				$bank_account_statement_transaction->date = $transaction->line21->transaction_date;
-				$bank_account_statement_transaction->valuta_date = $transaction->line21->valuta_date;
-				$bank_account_statement_transaction->amount = $transaction->line21->amount;
-				if ($transaction->line21->transaction_code_family == 35 && $transaction->line21->transaction_code_category == '001') {
-					$bank_account_statement_transaction->message = 'Interest';
-				} elseif ($transaction->line21->has_structured_message) {
-					if ($transaction->line21->structured_message_type == 114) {
-						$bank_account_statement_transaction->message = $transaction->line21->structured_message_full;
-						if (isset($transaction->line22->message)) {
-							$bank_account_statement_transaction->message = $bank_account_statement_transaction->message . $transaction->line22->message;
-						}
-					} else {
-						$bank_account_statement_transaction->structured_message = $transaction->line21->structured_message . '';
-					}
+				$bank_account_statement_transaction->date = $transaction->getTransactionDate()->format('Y-m-d');
+				$bank_account_statement_transaction->valuta_date = $transaction->getValutaDate()->format('Y-m-d');
+				$bank_account_statement_transaction->amount = $transaction->getAmount();
+				
+				$structured_message = $transaction->getStructuredMessage();
+				if (empty($structured_message)) {
+					$bank_account_statement_transaction->structured_message = $structured_message;
 				} else {
-					$bank_account_statement_transaction->message = $transaction->line21->message;
-					if (isset($transaction->line22->message)) {
-						$bank_account_statement_transaction->message = $bank_account_statement_transaction->message . $transaction->line22->message;
-					}
-					if (isset($transaction->line23->message)) {
-						$bank_account_statement_transaction->message = $bank_account_statement_transaction->message . $transaction->line23->message;
-					}
+					$bank_account_statement_transaction->message = $transaction->getMessage();				
 				}
 
-				if (isset($transaction->line22->other_account_bic)) {
-					$bank_account_statement_transaction->other_account_bic = $transaction->line22->other_account_bic;
-				}
-				if (isset($transaction->line23->other_account_number_and_currency)) {
-					$bank_account_statement_transaction->other_account_number = $transaction->line23->other_account_number_and_currency;
-				}
-				if (isset($transaction->line23->other_account_name)) {
-					$bank_account_statement_transaction->other_account_name = $transaction->line23->other_account_name;
-				}
+				$other_account = $transaction->getAccount();
+				$bank_account_statement_transaction->other_account_bic = $other_account->getBic();
+				$bank_account_statement_transaction->other_account_number = $other_account->getNumber();
+				$bank_account_statement_transaction->other_account_name = $other_account->getName();												
 				$bank_account_statement_transaction->save();
 			}
 
